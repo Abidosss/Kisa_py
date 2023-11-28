8218c53e8d4d6fe3fa9d7f3a9978151fc65cebfa
Abidosss <144780335+Abidosss@users.noreply.github.com>
2023-11-28 16:30:06 +0800

    4

--- flkT/app.py
+++ flkT/app.py
@@ -1,6 +1,6 @@
 # Refer: https://hackmd.io/@shaoeChen/rkpy9CGZz?type=view
 
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 
 app = Flask(__name__)
 
@@ -36,32 +36,42 @@ movies = [
 def index():
     return render_template('index.html', name=name, movies=movies)
 
-
 @app.route('/ad',methods=['GET','POST'])
 def play():
     if request.method == 'POST':
         return redirect(url_for('hello', username=request.form.get('username')))
     return render_template('playground.html')
 
-
-@app.route('/loginurl', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return redirect(url_for('hello', username=request.form.get('username')))
-
-    return render_template('login.html')
-
-
-@app.route('/hello/<username>')
-def hello(username):
-    return render_template('hello.html', username=username)
-
-
 @app.route('/cpaa', methods=['GET', 'POST'])
 def cp():
     if request.method == 'POST':
         return 'Hello ' + request.values['username']
     return render_template('cipher.html')
+
+
+
+@app.route('/loginurl', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if login_check(request.form['username'], request.form['password']):
+            flash('Login Success!')
+            return redirect(url_for('hello', username=request.form.get('username')))
+    return render_template('login.html')
+
+def login_check(username, password):
+    """登入帳號密碼檢核"""
+    if username == 'admin' and password == 'hello':
+        return True
+    else:
+        return False
+
+@app.route('/hello/<username>')
+def hello(username):
+    return render_template('hello.html', username=username)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
 
 
 app.debug = True
